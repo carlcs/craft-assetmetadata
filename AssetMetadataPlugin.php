5bdec25779bb1032be068a1955ce5f64caeee27d
carlcs <seelbach@gmx.de>
2016-06-08 02:08:17 +0200

    Prepare 2.1.4 release

--- AssetMetadataPlugin.php
+++ AssetMetadataPlugin.php
@@ -10,7 +10,7 @@ class AssetMetadataPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '2.1.3';
+        return '2.1.4';
     }
 
     public function getSchemaVersion()
