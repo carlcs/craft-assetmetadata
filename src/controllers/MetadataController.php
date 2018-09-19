880bf61ff43cc5497cbee74285abe9e68eb98c04
carlcs <seelbach@gmx.de>
2018-09-19 15:11:05 +0200

    Fixed Craft 3 migration

--- src/controllers/MetadataController.php
+++ src/controllers/MetadataController.php
@@ -5,8 +5,12 @@ namespace carlcs\assetmetadata\controllers;
 use carlcs\assetmetadata\fields\AssetMetadata;
 use carlcs\assetmetadata\Plugin;
 use Craft;
+use craft\db\Query;
 use craft\elements\Asset;
+use craft\helpers\ArrayHelper;
+use craft\helpers\Json;
 use craft\web\Controller;
+use yii\base\InvalidConfigException;
 use yii\web\BadRequestHttpException;
 use yii\web\Response;
 
@@ -39,5 +43,57 @@ class MetadataController extends Controller
         $value = Plugin::getInstance()->getMetadata()->getFieldValue($field, $asset);
 
         return $this->asJson($value);
+    }
+
+    /**
+     * Fixes a field’s subfields settings.
+     * Please configure this action with a config/asset-metadata-fix.php config file.
+     *
+     * @return string
+     */
+    public function actionFixSubfieldsMap(): string
+    {
+        $config = Craft::$app->getConfig()->getConfigFromFile('asset-metadata-fix');
+
+        $fieldHandle = $config['fieldHandle'];
+        $subfieldsMap = $config['subfieldsMap'];
+
+        $field = (new Query())
+            ->select(['settings'])
+            ->from(['{{%fields}}'])
+            ->where([
+                'handle' => $fieldHandle,
+                'type' => AssetMetadata::class,
+            ])
+            ->one();
+
+        if (!$field) {
+            throw new InvalidConfigException('Invalid field handle.');
+        }
+
+        $settings = Json::decode($field['settings']);
+
+        if (!is_array($settings['subfields'])) {
+            throw new InvalidConfigException('Field doesn’t have any subfields.');
+        }
+
+        $oldKeys = array_keys($settings['subfields']);
+        $mapKeys = array_keys($subfieldsMap);
+
+        if (!ArrayHelper::isSubset($oldKeys, $mapKeys)) {
+            throw new InvalidConfigException('Subfields map doesn’t map all existing keys.');
+        }
+
+        $newSubfields = [];
+        foreach ($settings['subfields'] as $oldKey => $subfield) {
+            $newSubfields[$subfieldsMap[$oldKey]] = $subfield;
+        }
+        $settings['subfields'] = $newSubfields;
+
+        Craft::$app->getDb()->createCommand()
+            ->update('{{%fields}}', ['settings' => Json::encode($settings)], ['handle' => $fieldHandle])
+            ->execute();
+
+        return 'Field updated.';
     }
 }
